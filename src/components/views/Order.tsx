f5b3e3a4b4cd904895f93685fdf1444c93fe9c2d
Priyanshu Panda <priyanshupanda202@proton.me>
2025-07-01 15:24:21 +0530

    Updated PO history table

--- src/components/views/Order.tsx
+++ src/components/views/Order.tsx
@@ -5,60 +5,45 @@ import { useEffect, useState } from 'react';
 import type { ColumnDef } from '@tanstack/react-table';
 import { formatDate } from '@/lib/utils';
 import DataTable from '../element/DataTable';
-
+import { Pill } from '../ui/pill';
 
 interface HistoryData {
-    indentDate: string;
-    date: string;
-    indentNo: string;
     poNumber: string;
-    product: string;
-    quantity: number;
-    rate: number;
-    uom: string;
     poCopy: string;
+    vendorName: string;
+    preparedBy: string;
+    approvedBy: string;
+    totalAmount: number;
+    status: 'Revised' | 'Not Recieved' | 'Recieved';
 }
 
 export default () => {
-    const { indentSheet, indentLoading } = useSheets();
+    const { poMasterLoading, poMasterSheet, indentSheet, receivedSheet } = useSheets();
 
     const [historyData, setHistoryData] = useState<HistoryData[]>([]);
 
     // Fetching table data
     useEffect(() => {
         setHistoryData(
-            indentSheet
-                .filter((sheet) => sheet.planned4 !== '' && sheet.actual4 !== '')
-                .map((sheet) => ({
-                    indentDate: formatDate(new Date(sheet.timestamp)),
-                    date: formatDate(new Date(sheet.actual4)),
-                    indentNo: sheet.indentNumber,
-                    poNumber: sheet.poNumber,
-                    product: sheet.productName,
-                    quantity: sheet.approvedQuantity,
-                    rate: sheet.approvedRate,
-                    uom: sheet.uom,
-                    poCopy: sheet.poCopy,
-                }))
+            poMasterSheet.map((sheet) => ({
+                approvedBy: sheet.approvedBy,
+                poCopy: sheet.pdf,
+                poNumber: sheet.poNumber,
+                preparedBy: sheet.preparedBy,
+                totalAmount: sheet.totalPoAmount,
+                vendorName: sheet.partyName,
+                status: indentSheet.map((s) => s.poNumber).includes(sheet.poNumber)
+                    ? receivedSheet.map((r) => r.poNumber).includes(sheet.poNumber)
+                        ? 'Recieved'
+                        : 'Not Recieved'
+                    : 'Revised',
+            }))
         );
     }, [indentSheet]);
 
     // Creating table columns
     const historyColumns: ColumnDef<HistoryData>[] = [
-        { accessorKey: 'indentDate', header: 'Indent Date' },
-        { accessorKey: 'date', header: 'PO Date' },
-        { accessorKey: 'indentNo', header: 'Indent No.' },
         { accessorKey: 'poNumber', header: 'PO Number' },
-        { accessorKey: 'product', header: 'Product' },
-        { accessorKey: 'quantity', header: 'Quantity' },
-        {
-            accessorKey: 'rate',
-            header: 'Rate',
-            cell: ({ row }) => {
-                return <>&#8377;{row.original.rate}</>;
-            },
-        },
-        { accessorKey: 'uom', header: 'UOM' },
         {
             accessorKey: 'poCopy',
             header: 'PO Copy',
@@ -73,20 +58,36 @@ export default () => {
                 );
             },
         },
+        { accessorKey: 'vendorName', header: 'Vendor Name' },
+        { accessorKey: 'preparedBy', header: 'Prepared By' },
+        { accessorKey: 'approvedBy', header: 'Approved By' },
+        {
+            accessorKey: 'totalAmount',
+            header: 'Amount',
+            cell: ({ row }) => {
+                return <>&#8377;{row.original.totalAmount}</>;
+            },
+        },
+        { accessorKey: 'status', header: 'Status',
+            cell: ({ row }) => {
+                const variant = row.original.status === "Not Recieved" ? "secondary" : row.original.status === "Recieved" ? "primary" : "default"
+                return <Pill variant={variant}>{row.original.status}</Pill>
+            }
+         },
     ];
 
     return (
         <div>
-                <Heading heading="Orders" subtext="View purchase orders" tabs>
-                    <Package2 size={50} className="text-primary" />
-                </Heading>
-                
-                    <DataTable
-                        data={historyData}
-                        columns={historyColumns}
-                        searchFields={['product', 'poNumber']}
-                        dataLoading={indentLoading}
-                    />
+            <Heading heading="PO History" subtext="View purchase orders">
+                <Package2 size={50} className="text-primary" />
+            </Heading>
+
+            <DataTable
+                data={historyData}
+                columns={historyColumns}
+                searchFields={['product', 'poNumber']}
+                dataLoading={poMasterLoading}
+            />
         </div>
     );
 };
